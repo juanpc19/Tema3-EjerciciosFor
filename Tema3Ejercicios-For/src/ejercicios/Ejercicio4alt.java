5487609243a864acc1e14e702bf1700dbbc9b55d
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2022-11-10 20:34:24 +0100

    dada

---
+++ Tema3Ejercicios-For/src/ejercicios/Ejercicio4alt.java
@@ -0,0 +1,36 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio4alt {
+
+	public static void main(String[] args) {
+		
+		
+		int numeroA;
+		
+		int numeroB;
+		
+		int mcdA=1;
+		
+		int mcdB=1;
+		
+		Scanner dogma = new Scanner(System.in);
+		
+		System.out.print("Introduzca un valor para numero A: ");
+		
+		numeroA = dogma.nextInt();
+		
+		System.out.print("Introduzca un valor para numero B: ");
+		
+		numeroB = dogma.nextInt();
+		
+		//no me deja entrar o ejecucion infinita probar for dentro de for
+		while (mcdA!=mcdB) {
+			
+			System.out.print("Introduzca un valor para numero B: ");
+			
+		}
+	}
+
+}
